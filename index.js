6809389ca75b08d85a9a1c69fd40798b0005a905
marnionrails <37088724+marnionrails@users.noreply.github.com>
2023-01-15 17:06:14 -0500

    make onclick function for whenever a square is clicked

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const result = document.querySelector("#result");
+  const displayCurrentPlayer = document.querySelector("#current-player");
+  let currentPlayer = 1;
 
-})
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+      alert("You have clicked square " + i);
+    };
+  }
+});
